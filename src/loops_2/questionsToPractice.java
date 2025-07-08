9fc079bf9ed955d41a93759e6c315eecbf23c760
Akdohe <akshaydohe17@gmail.com>
2025-07-08 23:57:59 +0530

    new commit for practice question for lec 4

--- src/loops_2/questionsToPractice.java
+++ src/loops_2/questionsToPractice.java
@@ -6,6 +6,7 @@ public class questionsToPractice {
     public static void main(String[] args) {
 //        Print the sum of first n natural numbers
 //        n=4 ;
+/*
         int sum= 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("please enter the desired number for the sum : ");
@@ -15,14 +16,33 @@ public class questionsToPractice {
         }
         System.out.println(sum); // sum always print outside of loop
 
+*/
 
 //    Q2 print the number of the table
+/*
         System.out.println("please enter the number for table");
+        Scanner sc = new Scanner(System.in);
         int counter = sc.nextInt();
 for (int i = 1; i<=10; i++){
     System.out.println((counter * i));
 }
+*/
+
+//    To print the even numbers
+        // logic: number / 2 = 0 as remender which is ;ike modulo = 0
+
+//        int mod = sc.nextInt();
+        System.out.println("please enter the value of n");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    for (int i = 0; i<=n; i++){
+    if ( i % 2 == 0) {
+        System.out.println(i + " ");
+    }
+    }
+    }
     }
 
 
-}
+
+
